07c2ea240132639b0c5f249afcb6ca83a83534b0
Mohd latef <github@email.sendithere.co>
2024-08-18 19:22:41 +0300

    gave up!

--- src/libs/contract.ts
+++ src/libs/contract.ts
@@ -2,7 +2,7 @@ import { Keypair, TransactionBuilder } from '@stellar/stellar-sdk'
 import stellar from '@stellar/typescript-wallet-sdk'
 import { Spec, Client } from '@stellar/stellar-sdk/contract'
 import { Server } from '@stellar/stellar-sdk/rpc'
-const rpc_url = 'https://soroban-testnet.stellar.org:443'
+const rpc_url = 'https://soroban-testnet.stellar.org'
 const network_passphrase = 'Test SDF Network ; September 2015'
 const CONTRACT_ID = import.meta.env.PUBLIC_CONTRACT_ID
 const PRIVATE_KEY = import.meta.env.OPERATOR_PRIVATE_KEY
@@ -24,19 +24,36 @@ const client = new Client(contractSpec, {
   rpcUrl: rpc_url,
 })
 
-export const logAction = async (
-  user: string,
-  action: string,
-  impact: number,
-) => {
-  try {
-    const tx = await client('log_action', { action, impact, user })
-    const result = contractSpec.funcResToNative('log_action', tx.result.retval)
-    console.log('Action logged:', result)
+// const server = new Server(rpc_url)
+// Submits a tx and then polls for its status until a timeout is reached.
 
-    return result
-  } catch (error) {
-    console.error('Error logging action:', error)
-    throw error
-  }
-}
+// export const logAction = async (
+//   user: string,
+//   action: string,
+//   impact: number,
+// ) => {
+//   try {
+//     const tx = await client.options.contractId
+//     const result = contractSpec.funcResToNative('log_action', tx.result.retval)
+//     console.log('Action logged:', result)
+
+//     return result
+//   } catch (error) {
+//     console.error('Error logging action:', error)
+//     throw error
+//   }
+// }
+// const testLogAction = async () => {
+//   try {
+//     const user = 'user_address_here'
+//     const action = 'recycling'
+//     const impact = 10
+
+//     const result = await logAction(user, action, impact)
+//     console.log('LogAction result:', result)
+//   } catch (error) {
+//     console.error('Error in testLogAction:', error)
+//   }
+// }
+
+// testLogAction()
