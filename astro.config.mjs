831bbf4e6ef4257eb3ef151f0dcb519548f4d523
Mohd latef <github@email.sendithere.co>
2024-08-18 10:22:51 +0300

    t1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,38 @@
-import { defineConfig } from 'astro/config';
-import Unfonts from 'unplugin-fonts/astro';
-import tailwind from '@astrojs/tailwind';
-import alpinejs from '@astrojs/alpinejs';
-import clerk from '@clerk/astro';
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import Unfonts from 'unplugin-fonts/astro'
+import tailwind from '@astrojs/tailwind'
+import alpinejs from '@astrojs/alpinejs'
+import clerk from '@clerk/astro'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
-import icon from "astro-icon";
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), clerk({
-    signInUrl: '/sign-in',
-    signUpUrl: '/sign-up',
-    afterSignInUrl: '/',
-    afterSignUpUrl: '/'
-  }), Unfonts({
-    fontsource: {
-      families: [{
-        name: 'inter',
-        variable: true,
-        weights: [400, 900]
-      }]
-    }
-  }), alpinejs(), icon()],
+  adapter: vercel(),
+  integrations: [
+    react(),
+    tailwind(),
+    clerk({
+      afterSignInUrl: '/',
+      afterSignUpUrl: '/',
+      signInUrl: '/sign-in',
+      signUpUrl: '/sign-up',
+    }),
+    Unfonts({
+      fontsource: {
+        families: [
+          {
+            name: 'inter',
+            variable: true,
+            weights: [400, 900],
+          },
+        ],
+      },
+    }),
+    alpinejs(),
+    icon(),
+  ],
   output: 'server',
-  adapter: vercel()
-});
+})
--- src/libs/contract.ts
+++ src/libs/contract.ts
@@ -1,8 +1,10 @@
-import { Keypair, TransactionBuilder } from "@stellar/stellar-sdk";
-import { Client } from '@stellar/stellar-sdk/contract';
+// import { Keypair, TransactionBuilder } from "@stellar/stellar-sdk";
+// import { Client } from '@stellar/stellar-sdk/contract';
 
-
-const CONTRACT_ID = import.meta.env.VITE_CONTRACT_ID
-const PRIVATE_KEY = import.meta.env.VITE_PRIVATE_KEY
-
-console.log(`Contract ID: ${CONTRACT_ID}`)
+const rpc_url = 'https://soroban-testnet.stellar.org:443'
+const network_passphrase = 'Test SDF Network ; September 2015'
+const CONTRACT_ID = import.meta.env.PUBLIC_CONTRACT_ID
+const PRIVATE_KEY = import.meta.env.OPERATOR_PRIVATE_KEY
+export const test = () => {
+  const contractId = CONTRACT_ID
+}
