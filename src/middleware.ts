c30cf8c9cc30b8c66424f0c7fe54d2fe92d9d46a
Mohd latef <github@email.sendithere.co>
2024-08-18 14:17:38 +0300

    phase 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,8 +6,6 @@ export const onRequest = clerkMiddleware((auth, context) => {
   const { redirectToSignIn, userId } = auth()
 
   if (!userId && isProtectedRoute(context.request)) {
-    // Add custom logic to run before redirecting
-
     return redirectToSignIn()
   }
 })
